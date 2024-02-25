1712134f444a42a713c7fd6963e5515469b9272f
Kelvin Ngan <nth1030@gmail.com>
2024-02-25 20:15:30 +0800

    Add a task bar and close button and style it

---
+++ scripts/toDoList.js
@@ -0,0 +1,22 @@
+function createCloseButton(){
+
+    // Question: Use querySelectorAll or getElementsByTagName? 
+    // Ans: querySelectorAll return a static node list while getElement return a live node list
+    // Further ans: Live node list can be appended but static cannot
+
+    const list = document.querySelectorAll("li")
+    .forEach((listItem) => {
+        const button = document.createElement('button');
+        button.className = "close";
+        button.addEventListener('click', () => {
+            console.log('clicked');
+        });
+        listItem.appendChild(button);
+        
+        //listItem.insertAdjacentElement('afterend', button);
+    })
+    
+    //console.log(list);
+}
+
+createCloseButton();
