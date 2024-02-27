f8b1118e0640a2ab6e066dda089a94739df84a65
Kelvin Ngan <nth1030@gmail.com>
2024-02-27 17:23:24 +0800

    add new task when add button is pressed

--- scripts/toDoList.js
+++ scripts/toDoList.js
@@ -1,44 +1,56 @@
-function createCloseButton(){
+const list = document.querySelectorAll("li")
+.forEach((listItem) => {
 
-    // Question: Use querySelectorAll or getElementsByTagName? 
-    // Ans: querySelectorAll return a static node list while getElement return a live node list
-    // Further ans: Live node list can be appended but static cannot
+    //checked the listitem when clicked
+    listItem.addEventListener('click', () => {
+        console.log('lisitem clicked');
+        listItem.classList.toggle('checked');
+        
+        //handle the close button
+        if(listItem.classList.contains("checked"))
+        {
+            button.classList.add("close-checked");
+        }
+        else
+        {
+            button.classList.remove("close-checked");
+        }
+    });
 
-    const list = document.querySelectorAll("li")
-    .forEach((listItem) => {
+    //Hide the item when clicking close button
+    const button = document.createElement('button');
+    button.className = "close";
+    button.textContent = "\u00D7";
+    button.addEventListener('click', () => {
+        listItem.style.display = "none";
 
-        //checked the listitem when clicked
-        listItem.addEventListener('click', () => {
-            console.log('lisitem clicked');
-            listItem.classList.toggle('checked');
-            
-            //handle the close button
-            if(listItem.classList.contains("checked"))
-            {
-                button.classList.add("close-checked");
-            }
-            else
-            {
-                button.classList.remove("close-checked");
-            }
-        });
+    });
+    listItem.appendChild(button);
+})
+    
 
-        //Hide the item when clicking close button
-        const button = document.createElement('button');
-        button.className = "close";
-        button.textContent = "\u00D7";
-        button.addEventListener('click', () => {
-            listItem.style.display = "none";
 
-        });
 
-        listItem.appendChild(button);
+//create item when clicking "add" button
+function AddTask(){
+    //create a new list item 
+    const list = document.createElement("li");
+    //get userinput
+    const userInput = document.querySelector("#myInput").value;
+    //console.log(userInput);
+    //set the new element value with the user input, append it to the list item
+    const newListItem = document.createTextNode(userInput);
+    list.appendChild(newListItem);
 
-  
-
-    })
+    //handle exception and append it into the eexisting list
+    if (userInput === "")
+    {
+        alert('Your input cannot be blank !');
+    }
+    else{
+        //Append to the existing list
+        document.querySelector("#myUL").appendChild(list);
+    }
+    
     
 }
-
-createCloseButton();
-
